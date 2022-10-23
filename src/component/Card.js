2f89decb1f4e1328c1101448635dddfbe66685f8
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-10-23 16:30:22 +1100

    Flip Card

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,13 +1,22 @@
 import React from "react";
 import "./card.css";
 export const Card = () => {
+  const [flip, setFlip] = React.useState(false);
+  // "this.classList.toggle('flipped');"
+
+  const handleOnClick = () => {
+    setFlip(!flip);
+    console.log(flip);
+  };
   return (
-    <div className="card">
-      <div className="card__face card__face--front">
-        <img src="https://picsum.photos/200/300" alt="card" />
-      </div>
-      <div className="card__face card__face--back">
-        <img src="https://picsum.photos/200/300" alt="card" />
+    <div class="flipCard">
+      <div class={flip ? "card flipped" : "card"} onClick={handleOnClick}>
+        <div class="side front">Keyword</div>
+        <div class="side back">
+          Definition
+          <br />
+          Explanation
+        </div>
       </div>
     </div>
   );
